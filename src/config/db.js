0858654d029e5a44046e5fac10bead5a46c6802e
jnsid <145208368+jnsid@users.noreply.github.com>
2026-08-05 19:44:37 +0530

    Backend setup

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+
+        console.log("Connected Successfully");
+    } catch (error) {
+        console.error("Connection Failed:", error.message);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
